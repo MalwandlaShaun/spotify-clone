41dd116b4da5f8620042d8167db441b00ae09106
Malwandla Hlungwani <hlungwanimalwandlashaun@gmail.com>
2022-06-29 16:11:49 +0200

    second commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 export default function Login() {
   const handleClick = async () => {
-    const client_id = "b10ac7cc459d474e961a6603c15da715";
-    const redirect_uri = "https://spotify-clone-api.netlify.app/";
+    const client_id = "d7b721449ad9446ab150ead412dcd3fb";
+    const redirect_uri = "https://malwandla_spotify_clone.com/";
     const api_uri = "https://accounts.spotify.com/authorize";
     const scope = [
       "user-read-private",
